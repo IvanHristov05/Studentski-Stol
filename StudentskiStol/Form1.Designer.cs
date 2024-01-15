15f848e992ce56c405d08bac30178c0828269b9d
VankataO5 <125656458+IvanHristov05@users.noreply.github.com>
2024-01-15 11:29:12 +0200

    Add files via upload

---
+++ StudentskiStol/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace StudentskiStol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(46, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "С купон";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(145, 32);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(90, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "На свободно";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Предястие   Избери от менюто";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = ">";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(42, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(280, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "= 0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(280, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "= 0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(49, 177);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(42, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(97, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = ">";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(46, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(154, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Основно   Избери от менюто";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(280, 251);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "= 0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(49, 248);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(42, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(97, 251);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = ">";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(46, 221);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(148, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Десерт   Избери от менюто";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(280, 322);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(22, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "= 0";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(49, 319);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(42, 20);
+            this.textBox4.TabIndex = 19;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(97, 322);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(13, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = ">";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(46, 292);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(153, 13);
+            this.label12.TabIndex = 17;
+            this.label12.Text = "Напитки   Избери от менюто";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(49, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Ти дължиш";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(201, 367);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 23);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "C";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Пържени картофи",
+            "Пържени картофи със сирене",
+            "Пилешки флейки",
+            "Панирани сирена",
+            "Паниран кашкавал"});
+            this.comboBox1.Location = new System.Drawing.Point(114, 102);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 21);
+            this.comboBox1.TabIndex = 24;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Мусака",
+            "Пълнени чушки",
+            "Пиле с ориз",
+            "Пилешка супа",
+            "Яхния"});
+            this.comboBox2.Location = new System.Drawing.Point(114, 177);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(160, 21);
+            this.comboBox2.TabIndex = 25;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Шоколадова торта",
+            "Чиизкейк",
+            "Суфле",
+            "Крем карамел",
+            "Сладолед"});
+            this.comboBox3.Location = new System.Drawing.Point(114, 248);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(160, 21);
+            this.comboBox3.TabIndex = 26;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Кока - Кола",
+            "Фанта",
+            "Спрайт",
+            "Натурален сок",
+            "Чай"});
+            this.comboBox4.Location = new System.Drawing.Point(114, 319);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(160, 21);
+            this.comboBox4.TabIndex = 27;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(348, 402);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+    }
+}
+
---
+++ StudentskiStol/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentskiStol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sum = 0;
+            
+            if (double.Parse(textBox1.Text)!=0)
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    sum += 4.20 * double.Parse(textBox1.Text);
+                    label3.Text = double.Parse(textBox1.Text) * 4.20 + "лв.";
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    sum += 4.70 * double.Parse(textBox1.Text);
+                    label3.Text = double.Parse(textBox1.Text) * 4.70 + "лв.";
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    sum += 6.70 * double.Parse(textBox1.Text);
+                    label3.Text = double.Parse(textBox1.Text) * 6.70 + "лв.";
+                }
+                else if (comboBox1.SelectedIndex == 3)
+                {
+                    sum += 4.30 * double.Parse(textBox1.Text);
+                    label3.Text = double.Parse(textBox1.Text) * 4.30 + "лв.";
+                }
+                else if (comboBox1.SelectedIndex == 4)
+                {
+                    sum += 4.00 * double.Parse(textBox1.Text);
+                    label3.Text = double.Parse(textBox1.Text) * 4.00 + "лв.";
+                }
+            }
+            if (double.Parse(textBox2.Text) != 0)
+            {
+                if (comboBox2.SelectedIndex == 0)
+                {
+                    sum += 7.10 * double.Parse(textBox2.Text);
+                    label4.Text = double.Parse(textBox2.Text) * 7.10 + "лв.";
+                }
+                else if (comboBox2.SelectedIndex == 1)
+                {
+                    sum += 6.70 * double.Parse(textBox2.Text);
+                    label4.Text = double.Parse(textBox2.Text) * 6.70 + "лв.";
+                }
+                else if (comboBox2.SelectedIndex == 2)
+                {
+                    sum += 8.00 * double.Parse(textBox2.Text);
+                    label4.Text = double.Parse(textBox2.Text) * 8.00 + "лв.";
+                }
+                else if (comboBox2.SelectedIndex == 3)
+                {
+                    sum += 8.40 * double.Parse(textBox2.Text);
+                    label4.Text = double.Parse(textBox2.Text) * 8.40 + "лв.";
+                }
+                else if (comboBox2.SelectedIndex == 4)
+                {
+                    sum += 6.50 * double.Parse(textBox2.Text);
+                    label4.Text = double.Parse(textBox2.Text) * 6.50 + "лв.";
+                }
+            }
+            if (double.Parse(textBox3.Text) != 0)
+            {
+                if (comboBox3.SelectedIndex == 0)
+                {
+                    sum += 4.40 * double.Parse(textBox3.Text);
+                    label7.Text = double.Parse(textBox3.Text) * 4.40 + "лв.";
+                }
+                else if (comboBox3.SelectedIndex == 1)
+                {
+                    sum += 5.70 * double.Parse(textBox3.Text);
+                    label7.Text = double.Parse(textBox3.Text) * 5.70 + "лв.";
+                }
+                else if (comboBox3.SelectedIndex == 2)
+                {
+                    sum += 4.70 * double.Parse(textBox3.Text);
+                    label7.Text = double.Parse(textBox3.Text) * 4.70 + "лв.";
+                }
+                else if (comboBox3.SelectedIndex == 3)
+                {
+                    sum += 3.80 * double.Parse(textBox3.Text);
+                    label7.Text = double.Parse(textBox3.Text) * 3.80 + "лв.";
+                }
+                else if (comboBox3.SelectedIndex == 4)
+                {
+                    sum += 4.00 * double.Parse(textBox3.Text);
+                    label7.Text = double.Parse(textBox3.Text) * 4.00 + "лв.";
+                }
+            }
+            if (double.Parse(textBox4.Text) != 0)
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    sum += 3.00 * double.Parse(textBox4.Text);
+                    label10.Text = double.Parse(textBox4.Text) * 3.00 + "лв.";
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    sum += 3.00 * double.Parse(textBox4.Text);
+                    label10.Text = double.Parse(textBox4.Text) * 3.00 + "лв.";
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    sum += 2.80 * double.Parse(textBox4.Text);
+                    label10.Text = double.Parse(textBox4.Text) * 2.80 + "лв.";
+                }
+                else if (comboBox1.SelectedIndex == 3)
+                {
+                    sum += 2.50 * double.Parse(textBox4.Text);
+                    label10.Text = double.Parse(textBox4.Text) * 2.50 + "лв.";
+                }
+                else if (comboBox1.SelectedIndex == 4)
+                {
+                    sum += 2.70 * double.Parse(textBox4.Text);
+                    label10.Text = double.Parse(textBox4.Text) * 2.70 + "лв.";
+                }
+            }
+            
+            MessageBox.Show("Ти дължиш: " + sum + "лв.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+            comboBox3.SelectedIndex = -1;
+            comboBox4.SelectedIndex = -1;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "1";
+            textBox2.Text = "1";
+            textBox3.Text = "1";
+            textBox4.Text = "1";
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            textBox4.Enabled = true;
+        }
+    }
+}
